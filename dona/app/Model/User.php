02e9853911d70334f1d98516712d61e1afbaed04
Shota <t.shota23@gmail.com>
2016-02-15 19:49:58 +0800

    新規登録時のバリデーション追加、パスワードのハッシュ化

--- dona/app/Model/User.php
+++ dona/app/Model/User.php
@@ -1,11 +1,18 @@
 <?php
-
 App::uses('AppModel', 'Model');
 App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 
+/**
+ * User Model
+ *
+ */
 class User extends AppModel {
-   
-    public function beforeSave($options = array()) {
+	 
+	 public $hasMany = array(
+        'Profile'
+    );
+
+public function beforeSave($options = array()) {
       if (isset($this->data[$this->alias]['password'])) {
           $passwordHasher = new BlowfishPasswordHasher();
           $this->data[$this->alias]['password'] = $passwordHasher->hash(
@@ -14,4 +21,38 @@ class User extends AppModel {
       }
       return true;
     }
+public $validate = array(
+	'username' => array(
+            'required' => array(
+                'rule' => 'notEmpty',
+                'message' => 'ユーザーネームを入力してください。'
+            ),
+        	'too_long' => array(
+        		'rule' => array('maxLength', '20'),
+        		'message' => '20文字以内で入力してください。'
+        	)
+        ),
+	'email' => array(
+            'isValid' => array(
+                'rule' => 'email',
+                'required' => true,
+                'message' => 'メールアドレスを入力してください。'
+            )
+        ),
+	'password' => array(
+            'too_short' => array(
+                'rule' => array('minLength', '6'),
+                'message' => '６文字以上のパスワードを入力してください。'
+            ),
+            'required' => array(
+                'rule' => array('notEmpty'),
+                'message' => 'パスワードを入力してください。'
+            ),
+        	'too_long' => array(
+        		'rule' => array('maxLength', '15'),
+        		'message' => '15文字以内のパスワードを入力してください。'
+        	)
+        ));
+
+
 }
